649ed926c11e72b3fb9b259b31ebc805277c660e
Igorcapatina <capatina7802@gmail.com>
2019-11-26 21:57:49 -0800

    first try

---
+++ src/com/udemy/igor/Main.java
@@ -0,0 +1,8 @@
+package com.udemy.igor;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println(AreaCalculator.area(5));
+        System.out.println(AreaCalculator.area(-1.0,4.0));
+    }
+}
